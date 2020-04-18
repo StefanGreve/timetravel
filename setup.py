f1c3a5c1560c7ebfb6249a5fecac767e66937957
Stefan Greve <greve.stefan@outlook.jp>
2020-04-18 20:15:12 +0200

    Improved setup procedure (move version to meta.json)

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 
+import json
 import shutil
 from setuptools import setup
 from src import utils
 
 from pathlib import Path
 
-# move settings file
+#region pre-setup procedures
+
 settings = 'settings.json'
-shutil.copy(
-    Path.cwd().joinpath('src').joinpath(settings), 
-    utils.path_settings('timetravel').joinpath(settings)
-)
+meta = 'meta.json'
+
+utils.copy_settings(meta)
+utils.copy_settings(settings)
+
+metadata = None
+with open(utils.path_settings('timetravel').joinpath(meta), encoding = "utf-8", mode = "r") as metadata:
+        metadata = json.loads(metadata.read())
+
+#endregion
 
 setup(
-    author = "Stefan Greve",
+    author = metadata['author'],
     keywords = "python cli collaboration",
-    name = 'timetravel',
-    version = "1.1",
+    name = metadata['name'],
+    version = metadata['version'],
     description = "Walltime utility script for collaboration.",
     long_description = utils.read_file("readme.md"),
     long_description_content_type = "text/markdown",
