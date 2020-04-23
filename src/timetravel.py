fde5d842ff1da6b8726a918d6d4d56cd5f1a7112
Stefan Greve <greve.stefan@outlook.jp>
2020-04-23 22:48:42 +0200

    Refactored code (lessons learned while working on the weather module)

--- src/timetravel.py
+++ src/timetravel.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import json
 import errno
 from datetime import date, datetime
 import utils
@@ -17,17 +16,16 @@ def print_date_now(timezone):
     click.echo(f"in {click.style(timezone)}.")  
 
 def print_version(ctx, param, value):
-    meta = utils.path_settings('timetravel').joinpath('meta.json')
-    meta_content = json.loads(utils.read_file(meta))
+    meta = utils.read_json(utils.path_settings('timetravel').joinpath('meta.json'))
     
     if not value or ctx.resilient_parsing:
         return
 
-    click.secho(f"Python {meta_content['name']} Version {meta_content['version']}", fg = 'yellow')
-    click.echo(f"Copyright (C) {date.today().year} {meta_content['author']}")
+    click.secho(f"\nPython {meta['name']} version {meta['version']}", fg = 'yellow')
+    click.echo(f"Copyright (C) {date.today().year} {meta['author']}")
     click.echo("License GPLv3: GNU GPL version 3 <https://gnu.org/licenses/gpl.html>")
     click.echo("This is free software: you are free to change and redistribute it.")
-    click.echo("There is NO WARRANTY, to the extent permitted by law.")
+    click.echo("There is NO WARRANTY, to the extent permitted by law.\n")
     ctx.exit()
 
 @click.group(invoke_without_command = True)
@@ -44,13 +42,11 @@ def cli(ctx, user, city):
 @cli.command()
 @click.pass_context
 def time(ctx):
-    """ Prints current date and time of a specified user. """
     user = ctx.obj['USER']
     city = ctx.obj['CITY']
     settings = ctx.obj['SETTINGS']
 
-    settings_content = json.loads(utils.read_file(settings))
-    name = settings_content['name'].get(user, 'error')
+    name = utils.read_json(settings)['name'].get(user, 'error')
 
     if city:
         raise NotImplementedError('You cannot use this option yet.')
@@ -67,16 +63,15 @@ def time(ctx):
 @cli.command()
 @click.pass_context
 def all(ctx):
-    """ Reads and lists all users from settings. """
-    settings = ctx.obj['SETTINGS']
+    users = utils.read_json(ctx.obj['SETTINGS'])['name']
 
-    with open(settings, encoding = "utf-8", mode = "r") as settings:
-        users = json.loads(settings.read())['name']
-
-        for index, user in enumerate(users):
-            click.secho(f"{(index+1):02}. ", fg = 'yellow', nl = False)
-            click.echo(user)
+    for index, user in enumerate(users):
+        click.secho(f"{(index+1):02}. ", fg = 'yellow', nl = False)
+        click.echo(user)
 
  
 if __name__ == '__main__':
-    cli(obj = {})
+    try:
+        cli(obj = {})
+    except KeyboardInterrupt:
+        pass
